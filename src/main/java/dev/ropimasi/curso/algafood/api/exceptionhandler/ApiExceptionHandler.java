93505ff63ce38b3071b45f222bc7e155eefffb7d
ropimasi <ropimasi@email.com>
2025-10-09 18:34:55 -0300

    Módulo 08 aula 15 concluída.

--- src/main/java/dev/ropimasi/curso/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/dev/ropimasi/curso/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,9 +3,9 @@ package dev.ropimasi.curso.algafood.api.exceptionhandler;
 import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import dev.ropimasi.curso.algafood.domain.exception.EntidadeEmUsoException;
 import dev.ropimasi.curso.algafood.domain.exception.EntidadeNaoEncontradaException;
 import dev.ropimasi.curso.algafood.domain.exception.NegocioException;
@@ -14,7 +14,7 @@ import dev.ropimasi.curso.algafood.domain.exception.NegocioException;
 
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e) {
 		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
@@ -40,13 +40,4 @@ public class ApiExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
 	}
 
-
-
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem("O tipo de mídia não é aceito.")
-				.build();
-
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
-	}
 }
