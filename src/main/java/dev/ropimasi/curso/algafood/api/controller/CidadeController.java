9c6e411bf45b7f29c8bafd3dbe510ffe2267ad28
ropimasi <ropimasi@email.com>
2025-10-09 17:41:59 -0300

    Módulo 12 aula 12 e 13 concluída.

--- src/main/java/dev/ropimasi/curso/algafood/api/controller/CidadeController.java
+++ src/main/java/dev/ropimasi/curso/algafood/api/controller/CidadeController.java
@@ -1,10 +1,13 @@
 package dev.ropimasi.curso.algafood.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import dev.ropimasi.curso.algafood.api.exceptionhandler.Problema;
 import dev.ropimasi.curso.algafood.domain.exception.EntidadeNaoEncontradaException;
 import dev.ropimasi.curso.algafood.domain.exception.EstadoNaoEncontradoException;
 import dev.ropimasi.curso.algafood.domain.exception.NegocioException;
@@ -80,5 +84,9 @@ public class CidadeController {
 	public void remover(@PathVariable Long cidadeId) {
 		cidadeCadastroService.excluir(cidadeId);
 	}
+
+
+
+
 
 }
---
+++ src/main/java/dev/ropimasi/curso/algafood/api/exceptionhandler/Problema.java
@@ -0,0 +1,17 @@
+package dev.ropimasi.curso.algafood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+
+
+
+@Getter
+@Builder
+public class Problema {
+
+	private LocalDateTime dataHora;
+	private String mensagem;
+
+}
