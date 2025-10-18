08c9d6709eb436bce788f17b633d5ba0ae80df3c
ropimasi <ropimasi@email.com>
2025-10-18 12:41:19 -0300

    Módulo 08 aula 24 concluída.

--- src/main/java/dev/ropimasi/curso/algafood/api/controller/RestauranteController.java
+++ src/main/java/dev/ropimasi/curso/algafood/api/controller/RestauranteController.java
@@ -3,9 +3,13 @@ package dev.ropimasi.curso.algafood.api.controller;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +21,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.ropimasi.curso.algafood.domain.exception.CozinhaNaoEncontradaException;
-import dev.ropimasi.curso.algafood.domain.exception.EntidadeNaoEncontradaException;
 import dev.ropimasi.curso.algafood.domain.exception.NegocioException;
 import dev.ropimasi.curso.algafood.domain.model.Restaurante;
 import dev.ropimasi.curso.algafood.domain.repository.RestauranteRepository;
@@ -81,10 +85,11 @@ public class RestauranteController {
 
 
 	@PatchMapping(value = "/{restauranteId}")
-	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+	public Restaurante atualizarParcial(@PathVariable Long restauranteId,
+			@RequestBody Map<String, Object> campos, HttpServletRequest request) {
 		Restaurante restauranteAtual = restauranteCadastroService.buscarOuFalhar(restauranteId);
 
-		merge(campos, restauranteAtual);
+		merge(campos, restauranteAtual, request);
 
 		return atualizar(restauranteId, restauranteAtual);
 	}
@@ -92,18 +97,33 @@ public class RestauranteController {
 
 
 	// Para o atualizarParcial().
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		ObjectMapper objectMaapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMaapper.convertValue(dadosOrigem, Restaurante.class);
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino,
+			HttpServletRequest request) {
+		ServletServerHttpRequest serverHttpRequest =new ServletServerHttpRequest(request);
+		
+		try {
+			ObjectMapper objectMaapper = new ObjectMapper();
+			objectMaapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMaapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true);
+			Restaurante restauranteOrigem = objectMaapper.convertValue(dadosOrigem, Restaurante.class);
 
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
 
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			/* Pega IllegalArgumentExceptionque tem rootcause como:
+			 * IgnoredPropertyException e UnrecognizedPropertyException
+			 * que por sua vez são subclasse de PropertyBindingException , então...
+			 * relançar como HttpMessageNotReadableException para ser tratada no ApiExceptionHandler. */
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+		}
 	}
 
 
