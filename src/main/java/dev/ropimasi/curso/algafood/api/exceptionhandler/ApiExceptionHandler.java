ddbcabb67c60d0fd2c8740e05538b90ba7cc3b0c
ropimasi <ropimasi@email.com>
2025-10-19 14:40:25 -0300

    Módulo 08 aula 27 concluída.

--- src/main/java/dev/ropimasi/curso/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/dev/ropimasi/curso/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -27,6 +27,26 @@ import dev.ropimasi.curso.algafood.domain.exception.NegocioException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleDemaisException(Exception ex, WebRequest request) {
+		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+		ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
+		String detail = "Houve um erro inespecífico no sistema. Tente novamente e se o "
+				+ "problema persistir, entre em contato com o administrador do sistema.";
+
+		// Importante colocar o printStackTrace (pelo menos por enquanto, que não estamos
+		// fazendo logging) para mostrar a stacktrace no console
+		// Se não fizer isso, você não vai ver a stacktrace de exceptions que seriam importantes
+		// para você durante, especialmente na fase de desenvolvimento
+		ex.printStackTrace();
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+
+
+
 	@Override
 	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
