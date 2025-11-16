d7960bbcbf130d9554e49cb988297d3a10f2adee
ropimasi <ropimasi@email.com>
2025-11-16 18:37:18 -0300

    Módulo 09 aula 11 concluída.

--- src/main/java/dev/ropimasi/curso/algafood/domain/model/Restaurante.java
+++ src/main/java/dev/ropimasi/curso/algafood/domain/model/Restaurante.java
@@ -45,7 +45,7 @@ public class Restaurante {
 
 	//@NotNull
 	//@NotEmpty
-	@NotBlank(message = "O nome do restaurante é obrigatório")
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
